6de116b6934d26db50b1d9e4d6859fc0c4ac132d
Jasmin Hidanovic <dexinho008@gmail.com>
2024-02-19 06:48:19 +0100

    moved chatbot to separate file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const chatbot = require("./chatbot");
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./pdfs");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 app.use(cors());
+
+app.post("/pdf", upload.single("pdfFile"), (req, res) => {
+  chatbot(req.file.path)
+});
 
 const PORT = 3000;
 
