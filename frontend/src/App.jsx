c4a83db051137640f57637d8c9c2a4e6696be41d
Jasmin Hidanovic <dexinho008@gmail.com>
2024-02-22 02:28:35 +0100

    added Message component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
+import { Box } from "@mui/material";
 import Upload from "./Components/Upload/Upload";
+import Message from "./Components/Message/Message";
+import { useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState({});
+  const [isLastMessage, setIsLastMessage] = useState(false);
+
   return (
     <>
-      <Upload />
+      <Box className="container">
+        <Box>
+          <Box className="flex flex-col gap-4 p-4">
+            {messages.map((message, index) => {
+              <Message
+                className="border rounded-2xl p-4"
+                message={message}
+                isLastMessage={
+                  index + 1 === messages.length && messages % 2 === 0
+                }
+              />;
+            })}
+            <Message messages={messages} />
+          </Box>
+          <Upload />
+        </Box>
+      </Box>
     </>
   );
 }
